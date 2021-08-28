cb69206f1b40eece8ed467d9b7d737b9327bc091
Brendhon <brendhon.e.c.m@gmail.com>
2021-08-28 14:23:47 -0300

    Testando o GET do coffee grower

--- src/helpers/mock/common.mock.ts
+++ src/helpers/mock/common.mock.ts
@@ -1,3 +1,4 @@
+import { CoffeeGrowerDTO } from 'src/coffee-grower/dto/coffee-grower.dto';
 import { DeleteResult, UpdateResult } from 'typeorm';
 
 export abstract class MockConstants {
@@ -8,6 +9,15 @@ export abstract class MockConstants {
   };
 
   static readonly MOCK_DELETE_SERVICE: DeleteResult = { affected: 1, raw: [] };
+
+  static readonly MOCK_COFFEE_GROWER: Omit<CoffeeGrowerDTO, 'farm'> = {
+    name: 'Moises',
+    email: 'moises@teste.com.br',
+    password: '12345',
+  };
+
+  static readonly INVALID_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY0OTU0MzhkLTk4NjYtNGU2ZC1iNWQ2LTQ4YjdhMDVjN2ExMiIsImlhdCI6MTYzMDE2Njk3NywiZXhwIjoxNjMwMzM5Nzc3fQ.rYAqmG3v6vXe_jDBG4u89ns_NQMU5ysGowkvrSAZIwM';
 }
 
 /**
--- test/coffee-grower.e2e-spec.ts
+++ test/coffee-grower.e2e-spec.ts
@@ -6,14 +6,18 @@ import { configService } from '../src/helpers/config/config.service';
 import { CoffeeGrowerModule } from '../src/coffee-grower/coffee-grower.module';
 import { Repository } from 'typeorm';
 import { CoffeeGrowerEntity } from '../src/coffee-grower/model/coffee-grower.entity';
+import { MockConstants } from '../src/helpers/mock/common.mock';
+import { AuthModule } from '../src/auth/auth.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let repository: Repository<CoffeeGrowerEntity>;
+  let coffeeGrower: any;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [
+        AuthModule,
         CoffeeGrowerModule,
         TypeOrmModule.forRoot(configService.getTypeOrmConfigForTest()),
       ],
@@ -22,6 +26,10 @@ describe('AppController (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
     repository = module.get('CoffeeGrowerEntityRepository');
+  });
+
+  beforeEach(async () => {
+    coffeeGrower = MockConstants.MOCK_COFFEE_GROWER;
   });
 
   afterEach(async () => {
@@ -33,16 +41,67 @@ describe('AppController (e2e)', () => {
   });
 
   describe('POST /coffee-grower', () => {
-    it('should create a coffee-grower', async () => {
+    it('should create a coffee grower', async () => {
       await supertest
         .agent(app.getHttpServer())
         .post('/coffee-grower')
-        .send({
-          name: 'Moises',
-          email: 'moises@teste.com.br',
-          password: '12345',
-        })
-        .expect(201);
+        .send(coffeeGrower)
+        .expect(201); // Deve retornar 201 - created
+    });
+
+    it('should throw BadRequestException', async () => {
+      // Salvando usuário no banco
+      await repository.save({ ...coffeeGrower });
+
+      await supertest
+        .agent(app.getHttpServer())
+        .post('/coffee-grower')
+        .send(coffeeGrower)
+        .expect(400); // Deve lançar o bad request pois usuário ja esta no banco
+    });
+  });
+
+  describe('GET /coffee-grower', () => {
+    it('should list all coffee growers', async () => {
+      await supertest
+        .agent(app.getHttpServer())
+        .get('/coffee-grower/all')
+        .expect(200); // Deve retornar 200 - OK
+    });
+
+    it('should list a coffee grower', async () => {
+      // Salvando as informações no banco para buscar dados
+      await supertest
+        .agent(app.getHttpServer())
+        .post('/coffee-grower')
+        .send(coffeeGrower);
+
+      // Realizando o login para pegar o token
+      const { body } = await supertest
+        .agent(app.getHttpServer())
+        .post('/auth/login')
+        .send(coffeeGrower);
+
+      await supertest
+        .agent(app.getHttpServer())
+        .get('/coffee-grower')
+        .set('Authorization', 'Bearer ' + body.data.access_token)
+        .expect(200); // Deve retornar 200 - OK
+    });
+
+    it('should throw NotFoundException', async () => {
+      await supertest
+        .agent(app.getHttpServer())
+        .get('/coffee-grower')
+        .set('Authorization', 'Bearer ' + MockConstants.INVALID_TOKEN)
+        .expect(404); // Deve retornar 404 - Usuário não encontrado
+    });
+
+    it('should throw UnauthorizedException if no auth token', async () => {
+      await supertest
+        .agent(app.getHttpServer())
+        .get('/coffee-grower')
+        .expect(401); // Deve retornar 401 - Sem token de autenticação
     });
   });
 });
