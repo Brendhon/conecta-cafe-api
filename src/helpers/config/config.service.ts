210e1084897236fa418897f27cef3c5cdbe2cee7
Brendhon <brendhon.e.c.m@gmail.com>
2021-09-25 13:38:45 -0300

    Alterando arquivo de configuração

--- src/helpers/config/config.service.ts
+++ src/helpers/config/config.service.ts
@@ -60,7 +60,7 @@ class ConfigService {
       password: this.getValue('POSTGRES_PASSWORD'),
       database: this.getValue('POSTGRES_DATABASE'),
       entities: ['src/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: false,
     };
   }
 }
